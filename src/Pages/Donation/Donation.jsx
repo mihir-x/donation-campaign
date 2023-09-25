5c55657e1dd133fb590c58be53f7b4a9572c88e6
mihir-x <ictlancerx@gmail.com>
2023-09-25 17:45:37 +0600

    added see all button click functionality in donation page

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -6,29 +6,36 @@ const Donation = () => {
 
     const [donated, setDonated] = useState([])
     const [isFound, setIsFound] = useState(false)
+    const [isSeeAllClicked, setIsSeeAllClicked] = useState(false)
 
-    useEffect(() =>{
+    useEffect(() => {
         const storedDonatedItems = JSON.parse(localStorage.getItem('donations'))
 
-        if(storedDonatedItems){
+        if (storedDonatedItems) {
             setDonated(storedDonatedItems)
             setIsFound(true)
         }
-    },[])
-    console.log(donated)
-    console.log(isFound)
+    }, [])
+    console.log(isSeeAllClicked);
 
     return (
         <div>
             {
-                isFound ? <div className="grid grid-cols-1 md:grid-cols-2 gap-5 my-2 md:mb-6 lg:mb-16 p-2">
-                    {
-                        donated.map(item => <DonatedCard key={item.key} item={item}></DonatedCard>)
-                    }
+                isFound ? <div>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-5 my-2 md:mb-6 lg:mb-10 p-2">
+                        {
+                            isSeeAllClicked ? donated.map(item => <DonatedCard key={item.key} item={item}></DonatedCard>): donated.slice(0,4).map(item => <DonatedCard key={item.key} item={item}></DonatedCard>)
+                        }
+                    </div>
+                    <div className=" flex justify-center mb-5 md:mb-10">
+                        {
+                            (donated.length > 4 && isSeeAllClicked === false) ? <button onClick={() => setIsSeeAllClicked(true)} className="py-2 px-3 font-semibold text-white bg-[#009444] rounded-md">See All</button> : ''
+                        }
+                    </div>
                 </div>
-                : <p>no item here</p>
+                    : <div className="h-screen md:h-[50vh] flex justify-center items-center"><p>no item here</p></div>
             }
-        </div>
+        </div >
     );
 };
 
