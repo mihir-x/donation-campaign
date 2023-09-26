558570255dae798b69694b79eba6b50f9b46a361
mihir-x <ictlancerx@gmail.com>
2023-09-26 12:38:49 +0600

    added overlay background image just in banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,19 +1,21 @@
 import PropTypes from 'prop-types'
 
 
-const Banner = ({handleSearch}) => {
-    
+const Banner = ({ handleSearch }) => {
+
 
     return (
-        <div className=" p-2 bg-[#fffffff2] h-[50vh] flex flex-col justify-center text-center space-y-2 md:space-y-4 lg:space-y-10">
-            <div>
-                <h1 className="text-xl md:text-3xl lg:text-5xl font-bold">I Grow By Helping People In Need</h1>
-            </div>
-            <div>
-                <form onSubmit={handleSearch}>
-                    <input type="text" name="search" id="" placeholder="Search Here..." className="border py-3 pl-5 border-[#DEDEDE] md:w-96" />
-                    <input type="submit" value="Search" className="text-white bg-[#FF444A] px-5 py-3 font-semibold rounded-r-md" />
-                </form>
+        <div className=" bg-[url('./donation.jpg')] bg-cover bg-[#fffffff2] h-[50vh] flex flex-col justify-center text-center">
+            <div className=' space-y-2 md:space-y-4 lg:space-y-10 w-full h-full bg-slate-100/90 backdrop-brightness-100 flex flex-col justify-center text-center'>
+                <div>
+                    <h1 className="text-xl md:text-3xl lg:text-5xl font-bold">I Grow By Helping People In Need</h1>
+                </div>
+                <div>
+                    <form onSubmit={handleSearch}>
+                        <input type="text" name="search" id="" placeholder="Search Here..." className="border py-3 pl-5 border-[#DEDEDE] md:w-96" />
+                        <input type="submit" value="Search" className="text-white bg-[#FF444A] px-5 py-3 font-semibold rounded-r-md" />
+                    </form>
+                </div>
             </div>
         </div>
     );
