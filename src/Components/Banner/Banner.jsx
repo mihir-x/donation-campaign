0a52d7db86cbaed88dc419424ade46472b59f74b
mihir-x <ictlancerx@gmail.com>
2023-09-26 22:55:09 +0600

    changed banner background image overlay style

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -5,8 +5,8 @@ const Banner = ({ handleSearch }) => {
 
 
     return (
-        <div className=" bg-[url('https://i.ibb.co/qmhrpkw/donation.jpg')] bg-cover bg-[#fffffff2] h-[50vh] flex flex-col justify-center text-center">
-            <div className=' space-y-2 md:space-y-4 lg:space-y-10 w-full h-full bg-slate-100/90 backdrop-brightness-100 flex flex-col justify-center text-center'>
+        <div className=" bg-[url('https://i.ibb.co/qmhrpkw/donation.jpg')] bg-cover bg-[#fffffff2] bg-blend-overlay h-[50vh] flex flex-col justify-center text-center">
+            <div className=' space-y-2 md:space-y-4 lg:space-y-10 w-full h-full flex flex-col justify-center text-center'>
                 <div>
                     <h1 className="text-xl md:text-3xl lg:text-5xl font-bold">I Grow By Helping People In Need</h1>
                 </div>
