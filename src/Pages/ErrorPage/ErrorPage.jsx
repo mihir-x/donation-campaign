fb21aec4a65eded8a6f778520322a411d31a998b
mihir-x <ictlancerx@gmail.com>
2023-09-27 19:03:15 +0600

    fixed error text

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -5,9 +5,9 @@ const ErrorPage = () => {
     return (
         <div className="h-screen flex flex-col-reverse md:flex-row justify-center items-center p-2">
             <div className="text-center">
-                <h1 className=" text-3xl md:text-5xl lg:text-7xl font-bold">404</h1>
+                <h1 className=" text-2xl md:text-5xl lg:text-7xl font-bold">404</h1>
                 
-                <h2 className="text-2xl font-semibold">page not found</h2>
+                <h2 className="text-lg md:text-2xl font-semibold">Ooops! You made a wrong turn</h2>
                 <Link to={-1}><button className="p-2 bg-blue-600 rounded-md font-bold text-white mt-4">Go Back</button></Link>
             </div>
             <figure>
